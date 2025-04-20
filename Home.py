17bda7f8cd16988cac41ce05d6b1873163714e6b
Mervin50 <167336864+Mervin50@users.noreply.github.com>
2025-04-20 18:38:06 +0200

    Initial deployment

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+# st.write("# Welcome to Streamlit! 👋")
+
+st.markdown("# Welcome to Gurgaon Real Estate!👋👋")
+
+# st.sidebar.success("Select a demo above.")
+
+
+
+
